ea3809e90b6c2e8b8749be0e06a2c8eab04fcf0b
limjeewoo <limjeewoo123@gmail.com>
2025-12-18 17:57:50 +0900

    배포완성
    출력 결과 수정

--- src/main/java/site/metacoding/awsv2/HelloController.java
+++ src/main/java/site/metacoding/awsv2/HelloController.java
@@ -19,6 +19,6 @@ public class HelloController {
             log.warn("/aws/v2 이 호출되었어요. warn 로그 #####################################");
         }
 
-        return "<h1>aws v2</h1>";
+        return "<h1>배포완성-임지우</h1>";
     }
 }
